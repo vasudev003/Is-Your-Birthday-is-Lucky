754aa611440a06fc49f174f0e5cd6ef5a739ddcf
vasudev003 <vasudevpeshwani.vp@gmail.com>
2022-01-19 14:04:47 +0530

    update

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 const DateOfBirth = document.querySelector("#date-of-birth");
 const LuckyNumber = document.querySelector("#lucky-number");
 const CheckNumberButton = document.querySelector("#check-number");
+const output = document.querySelector("#txtArea");
+function CompareValues (add, LuckyNumber) {
+    if(add%LuckyNumber===0){
+        output.innerText="your BD IS LUCKy";
+    }
+    else{
+        output.innerText="your BD is not so lucky";
+    }
 
-CheckNumberButton.addEventListener('click', function getvalues() {
-    console.log(DateOfBirth.value, LuckyNumber.value );
-} )
+}
+
+function CheckBirthdatIsLucky(){
+    const DOB = DateOfBirth.value;
+    const add = calculateadd(DOB);
+    if(add&&DOB)
+    CompareValues(add,LuckyNumber.value)
+    output.innerText="Please Enter both fields 👿";
+
+}
+
+function calculateadd(DOB) {
+    DOB = DOB.replaceAll("-", "");
+    let add = 0;
+    for (let i=0; i<DOB.length; i++){
+        add = add + Number(DOB.charAt(i));
+    }
+    return add;
+}
+
+CheckNumberButton.addEventListener('click', CheckBirthdatIsLucky )
